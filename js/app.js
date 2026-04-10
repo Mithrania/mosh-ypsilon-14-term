618043529f4fafc7bc3f732ea1be630e3de0b305
Mithrania <jack.currie@outlook.com>
2026-04-10 13:58:28 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-
 import { navGuard, navGuardAdmin } from './navlock.js';
 import context from './ctx.js';
 import {
@@ -53,119 +52,125 @@ window.addEventListener('click', function (ev) {
 const menuHome = async function () {
     await navGuard(async () => {
         const menu = getEmptyMenu()
-        await createTypeWriterMenu(menu, "- DIAGNOSTICA", menuDiagnostics)
-        await createTypeWriterMenu(menu, "- REGISTRO", menuSchedule)
-        await createTypeWriterMenu(menu, "- CONTROLLI", menuControls)
-        await createTypeWriterMenu(menu, "- PERSONALE", menuRoster)
-        await createTypeWriterMenu(menu, "- COMUNICAZIONI", menuComms)
+        await createTypeWriterMenu(menu, "- DIAGNOSTICS", menuDiagnostics)
+        await createTypeWriterMenu(menu, "- SCHEDULE", menuSchedule)
+        await createTypeWriterMenu(menu, "- CONTROLS", menuControls)
+        await createTypeWriterMenu(menu, "- ROSTER", menuRoster)
+        await createTypeWriterMenu(menu, "- COMMUNICATIONS", menuComms)
     })
 }
 
-// HOME -> DIAGNOSTICA
+// HOME -> DIAGNOSTICS
 const menuDiagnostics = async function () {
     await navGuard(async () => {
         const menu = getEmptyMenu()
-        await createTypeWriterMenu(menu, "- MAPPA", menuMap)
-        await createTypeWriterMenu(menu, "- STATO", menuStatus)
-        await createTypeWriterMenu(menu, "< INDIETRO", menuHome)
+        await createTypeWriterMenu(menu, "- MAP", menuMap)
+        await createTypeWriterMenu(menu, "- STATUS", menuStatus)
+        await createTypeWriterMenu(menu, "< BACK", menuHome)
     })
 }
 
-// HOME -> DIAGNOSTICA -> MAPPA
+// HOME -> DIAGNOSTICS -> MAP
 const menuMap = async function () {
     await navGuard(async () => {
         const menu = getEmptyMenu()
         const img = createImageElement('img/map.png', 'stationmap')
         menu.appendChild(img)
-        await createTypeWriterMenu(menu, "< INDIETRO", menuDiagnostics)
+        await createTypeWriterMenu(menu, "< BACK", menuDiagnostics)
     })
 }
 
-// HOME -> DIAGNOSTICA -> STATO
+// HOME -> DIAGNOSTICS -> STATUS
 const menuStatus = async function () {
     await navGuard(async () => {
         const menu = getEmptyMenu()
         await createTypeWriterText(menu, "SYSTEM CHECK...", { style: `font-size: 90%` })
-        await createTypeWriterText(menu, "ATTENZIONE: FILTRI DELL'ARIA SOSTITUITI 455 GIORNI FA", { style: `color: yellow; font-size: 90%` })
-        await createTypeWriterText(menu, "ATTENZIONE: DOCCIA N.5 FUORI SERVIZIO DA 1 GIORNO", { style: `color: red; font-size: 90%` })
-        await createTypeWriterText(menu, "ATTENZIONE: ULTIMA MANUTENZIONE ASCENSORE MINIERA 455 GIORNI FA", { style: `color: yellow; font-size: 90%` })
-        await createTypeWriterText(menu, "ATTENZIONE: FLUSSO ARIA 82% (NON OTTIMO: SOSTITUIRE FILTRI E CONTROLLARE OSTRUZIONI CONDOTTI)", { style: `color: red; font-size: 90%` })
+
+        await createTypeWriterText(menu, "WARNING: AIR FILTERS REPLACED 455 DAYS AGO", { style: `color: yellow; font-size: 90%` })
+
+        await createTypeWriterText(menu, "WARNING: SHOWER #5 OUT OF SERVICE FOR 1 DAY", { style: `color: red; font-size: 90%` })
+
+        await createTypeWriterText(menu, "WARNING: MINING ELEVATOR LAST MAINTENANCE 455 DAYS AGO", { style: `color: yellow; font-size: 90%` })
+
+        await createTypeWriterText(menu, "WARNING: AIRFLOW AT 82% (NOT OPTIMAL: REPLACE FILTERS AND CHECK DUCT BLOCKAGES)", { style: `color: red; font-size: 90%` })
+
         if (!isLifeSupportOff() && !isSelfDestructActivated()) {
-            await createTypeWriterText(menu, "[TUTTI I SISTEMI OPERANO IN CONDIZIONI ACCETTABILI]", { style: `font-size: 90%` })
+            await createTypeWriterText(menu, "[ALL SYSTEMS OPERATING UNDER ACCEPTABLE CONDITIONS]", { style: `font-size: 90%` })
         } else {
             if (isLifeSupportOff())
-                await createTypeWriterText(menu, "ATTENZIONE: SUPPORTO VITALE DISATTIVATO!", { style: `color: red; font-size: 90%` })
+                await createTypeWriterText(menu, "WARNING: LIFE SUPPORT DISABLED!", { style: `color: red; font-size: 90%` })
+
             if (isSelfDestructActivated())
-                await createTypeWriterText(menu, "ATTENZIONE: SEQUENZA DI AUTO DISTRUZIONE ATTIVATA!", { style: `color: red; font-size: 90%` })
+                await createTypeWriterText(menu, "WARNING: SELF-DESTRUCT SEQUENCE ACTIVATED!", { style: `color: red; font-size: 90%` })
         }
-        await createTypeWriterMenu(menu, "< INDIETRO", menuDiagnostics)
+
+        await createTypeWriterMenu(menu, "< BACK", menuDiagnostics)
     })
 }
 
-// HOME -> REGISTRO
+// HOME -> SCHEDULE
 const menuSchedule = async function () {
     await navGuard(async () => {
         let menu = getEmptyMenu()
         menu.innerHTML = scheduleData
-        await createTypeWriterMenu(menu, "< INDIETRO", menuHome)
+        await createTypeWriterMenu(menu, "< BACK", menuHome)
     })
 }
 
-// HOME -> CONTROLLI
+// HOME -> CONTROLS
 const menuControls = async function () {
     await navGuard(async () => {
         const menu = getEmptyMenu()
-        await createTypeWriterMenu(menu, "- PORTELLONI", menuAirlocks)
-        await createTypeWriterMenu(menu, "- DOCCE", menuShowers)
-        await createTypeWriterMenu(menu, "- SISTEMA [A]", menuSystem)
-        await createTypeWriterMenu(menu, "< INDIETRO", menuHome)
+        await createTypeWriterMenu(menu, "- AIRLOCKS", menuAirlocks)
+        await createTypeWriterMenu(menu, "- SHOWERS", menuShowers)
+        await createTypeWriterMenu(menu, "- SYSTEM [A]", menuSystem)
+        await createTypeWriterMenu(menu, "< BACK", menuHome)
     })
 }
 
-// HOME -> CONTROLLI -> PORTELLONI
+// HOME -> CONTROLS -> AIRLOCKS
 const menuAirlocks = async function () {
     await navGuard(async () => {
         const menu = getEmptyMenu()
-        await createTypeWriterMenu(menu, `- BAIA 1 [${getAirlockStatus(context.lock_docking_bay_1)}]`, toggleAirlock1)
-        await createTypeWriterMenu(menu, `- BAIA 2 [${getAirlockStatus(context.lock_docking_bay_2)}]`, toggleAirlock2)
-        await createTypeWriterMenu(menu, `- ASCENSORE MINIERA [${getAirlockStatus(context.lock_mineshaft)}]`, toggleMineshaft)
-        await createTypeWriterMenu(menu, "< INDIETRO", menuControls)
+        await createTypeWriterMenu(menu, `- BAY 1 [${getAirlockStatus(context.lock_docking_bay_1)}]`, toggleAirlock1)
+        await createTypeWriterMenu(menu, `- BAY 2 [${getAirlockStatus(context.lock_docking_bay_2)}]`, toggleAirlock2)
+        await createTypeWriterMenu(menu, `- MINING ELEVATOR [${getAirlockStatus(context.lock_mineshaft)}]`, toggleMineshaft)
+        await createTypeWriterMenu(menu, "< BACK", menuControls)
     })
 }
 
-// HOME -> CONTROLLI -> DOCCE
+// HOME -> CONTROLS -> SHOWERS
 const menuShowers = async function () {
     await navGuard(async () => {
         const menu = getEmptyMenu()
         for (let i = 0; i < context.showers.length; i++) {
             const s = context.showers[i]
-            await createTypeWriterMenu(menu, `- DOCCIA ${i + 1} [${getOnOffStatus(s)}]`, () => toggleShower(i))
+            await createTypeWriterMenu(menu, `- SHOWER ${i + 1} [${getOnOffStatus(s)}]`, () => toggleShower(i))
         }
-        await createTypeWriterMenu(menu, "< INDIETRO", menuControls)
+        await createTypeWriterMenu(menu, "< BACK", menuControls)
     })
 }
 
-// HOME -> CONTROLLI -> SISTEMA [A]
-// Questo menu' necessita dei permessi di admin
+// HOME -> CONTROLS -> SYSTEM [A]
 const menuSystem = async function () {
     await navGuardAdmin(async () => {
         const menu = getEmptyMenu()
-        await createTypeWriterText(menu, `[ACCESSO CONSENTITO, BENTORNATA, SONYA]`, { style: `color: green` })
-        await createTypeWriterMenu(menu, `- SUPPORTO VITALE`, menuLifeSupport)
-        await createTypeWriterMenu(menu, `- AUTO DISTRUZIONE`, menuSelfDestruct)
-        await createTypeWriterMenu(menu, "< INDIETRO", menuControls)
+        await createTypeWriterText(menu, `[ACCESS GRANTED, WELCOME BACK, SONYA]`, { style: `color: green` })
+        await createTypeWriterMenu(menu, `- LIFE SUPPORT`, menuLifeSupport)
+        await createTypeWriterMenu(menu, `- SELF DESTRUCT`, menuSelfDestruct)
+        await createTypeWriterMenu(menu, "< BACK", menuControls)
     })
 }
 
-// HOME -> CONTROLLI -> SISTEMA [A] -> SUPPORTO VITALE
+// HOME -> CONTROLS -> SYSTEM [A] -> LIFE SUPPORT
 const menuLifeSupport = async function () {
     await navGuardAdmin(async () => {
         const menu = getEmptyMenu()
         if (isLifeSupportOff())
-            await createTypeWriterText(menu, `[ATTENZIONE: SUPPORTO VITALE DISATTIVATO]`, { style: `color: red` })
+            await createTypeWriterText(menu, `[WARNING: LIFE SUPPORT DISABLED]`, { style: `color: red` })
 
-        await createTypeWriterMenu(menu, `- SUPPORTO VITALE [${getOnOffStatus(context.life_support)}]`, menuDisableLifeSupport)
-        await createTypeWriterMenu(menu, "< INDIETRO", menuControls)
+        await createTypeWriterMenu(menu, `- LIFE SUPPORT [${getOnOffStatus(context.life_support)}]`, menuDisableLifeSupport)
+        await createTypeWriterMenu(menu, "< BACK", menuControls)
     })
 }
 
@@ -175,10 +180,11 @@ const menuDisableLifeSupport = async function () {
             const menu = getEmptyMenu()
             await createTypeWriterText(
                 menu,
-                `[ATTENZIONE, DISABILITARE IL SUPPORTO VITALE SENZA AUTORIZZAZIONE È CONTRO LE POLICY DI SICUREZZA. ASSICURATEVI DI COMPILARE IL MODULO 077-X24 IN OGNI SUO CAMPO E INVIARLO AL SUPERVISORE.]`,
+                `[WARNING, DISABLING LIFE SUPPORT WITHOUT AUTHORIZATION VIOLATES SAFETY POLICIES. ENSURE FORM 077-X24 IS COMPLETED AND SUBMITTED TO THE SUPERVISOR.]`,
                 { style: `color: red` })
-            await createTypeWriterMenu(menu, "- CONFERMA", () => { context.life_support = false; menuLifeSupport() })
-            await createTypeWriterMenu(menu, "< INDIETRO", menuControls)
+
+            await createTypeWriterMenu(menu, "- CONFIRM", () => { context.life_support = false; menuLifeSupport() })
+            await createTypeWriterMenu(menu, "< BACK", menuControls)
         } else {
             context.life_support = true
             menuLifeSupport()
@@ -186,70 +192,74 @@ const menuDisableLifeSupport = async function () {
     })
 }
 
-// HOME -> CONTROLLI -> SISTEMA [A] -> AUTO DISTRUZIONE
+// HOME -> CONTROLS -> SYSTEM [A] -> SELF DESTRUCT
 const menuSelfDestruct = async function () {
     await navGuardAdmin(async () => {
         const menu = getEmptyMenu()
         if (isSelfDestructActivated()) {
             await createTypeWriterText(menu,
-                `[SEQUENZA AUTO DISTRUZIONE ATTIVATA. LA STAZIONE DETONERÀ IN 10 MINUTI. ABBANDONARE LA STAZIONE.]`,
+                `[SELF-DESTRUCT SEQUENCE ACTIVATED. THE STATION WILL DETONATE IN 10 MINUTES. EVACUATE IMMEDIATELY.]`,
                 { style: `color: red` })
-            await createTypeWriterMenu(menu, `CONFERMA [${getConfirmationStr(context.self_destruct_confirm[0])}]`, () => toggleSelfDestructConfirm(0))
-            await createTypeWriterMenu(menu, `CONFERMA [${getConfirmationStr(context.self_destruct_confirm[1])}]`, () => toggleSelfDestructConfirm(1))
-            await createTypeWriterMenu(menu, `CONFERMA [${getConfirmationStr(context.self_destruct_confirm[2])}]`, () => toggleSelfDestructConfirm(2))
-            await createTypeWriterMenu(menu, "< INDIETRO", menuControls)
+
+            await createTypeWriterMenu(menu, `CONFIRM [${getConfirmationStr(context.self_destruct_confirm[0])}]`, () => toggleSelfDestructConfirm(0))
+            await createTypeWriterMenu(menu, `CONFIRM [${getConfirmationStr(context.self_destruct_confirm[1])}]`, () => toggleSelfDestructConfirm(1))
+            await createTypeWriterMenu(menu, `CONFIRM [${getConfirmationStr(context.self_destruct_confirm[2])}]`, () => toggleSelfDestructConfirm(2))
+            await createTypeWriterMenu(menu, "< BACK", menuControls)
         } else {
             await createTypeWriterText(menu,
-                `[ATTENZIONE, IL PROCESSO DI AUTODISTRUZIONE DETONERÀ LA STAZIONE DOPO 10 MINUTI DALL'ATTIVAZIONE. SE CONFERMATO IL PROCESSO DIVENTERÀ IRREVERSIBILE DOPO 5 MINUTI.]`,
+                `[WARNING, THE SELF-DESTRUCT PROCESS WILL DETONATE THE STATION 10 MINUTES AFTER ACTIVATION. ONCE CONFIRMED, IT WILL BECOME IRREVERSIBLE AFTER 5 MINUTES.]`,
                 { style: `color: red` })
-            await createTypeWriterMenu(menu, `CONFERMA [${getConfirmationStr(context.self_destruct_confirm[0])}]`, () => toggleSelfDestructConfirm(0))
-            await createTypeWriterMenu(menu, `CONFERMA [${getConfirmationStr(context.self_destruct_confirm[1])}]`, () => toggleSelfDestructConfirm(1))
-            await createTypeWriterMenu(menu, `CONFERMA [${getConfirmationStr(context.self_destruct_confirm[2])}]`, () => toggleSelfDestructConfirm(2))
-            await createTypeWriterMenu(menu, "< INDIETRO", menuControls)
+
+            await createTypeWriterMenu(menu, `CONFIRM [${getConfirmationStr(context.self_destruct_confirm[0])}]`, () => toggleSelfDestructConfirm(0))
+            await createTypeWriterMenu(menu, `CONFIRM [${getConfirmationStr(context.self_destruct_confirm[1])}]`, () => toggleSelfDestructConfirm(1))
+            await createTypeWriterMenu(menu, `CONFIRM [${getConfirmationStr(context.self_destruct_confirm[2])}]`, () => toggleSelfDestructConfirm(2))
+            await createTypeWriterMenu(menu, "< BACK", menuControls)
         }
     })
 }
 
-// HOME -> PERSONALE
+// HOME -> ROSTER
 const menuRoster = async function () {
     await navGuard(async () => {
         const menu = getEmptyMenu()
         menu.innerHTML = rosterData
-        await createTypeWriterMenu(menu, "< INDIETRO", menuHome)
+        await createTypeWriterMenu(menu, "< BACK", menuHome)
     })
 }
 
-// HOME -> COMUNICAZIONI
+// HOME -> COMMUNICATIONS
 const menuComms = async function () {
     await navGuard(async () => {
         const menu = getEmptyMenu()
-        await createTypeWriterText(menu, `SCANSIONE NAVI IN PROSSIMITÀ...`)
-        await createTypeWriterMenu(menu, `- CONTATTO RSV THE HERACLES`, () => menuContact('RSV THE HERACLES', false))
-        await createTypeWriterMenu(menu, `- CONTATTO IMV THE TEMPEST`, () => menuContact('IMV THE TEMPEST', true))
-        await createTypeWriterMenu(menu, "< INDIETRO", menuHome)
+        await createTypeWriterText(menu, `SCANNING NEARBY SHIPS...`)
+        await createTypeWriterMenu(menu, `- CONTACT RSV THE HERACLES`, () => menuContact('RSV THE HERACLES', false))
+        await createTypeWriterMenu(menu, `- CONTACT IMV THE TEMPEST`, () => menuContact('IMV THE TEMPEST', true))
+        await createTypeWriterMenu(menu, "< BACK", menuHome)
     })
 }
 
-// HOME -> COMUNICAZIONI -> <NAVE>
+// HOME -> COMMUNICATIONS -> <SHIP>
 const menuContact = async function (ship, success) {
     await navGuard(async () => {
         const menu = getEmptyMenu()
-        await createTypeWriterText(menu, `INVIO SEGNALE DI CONTATTO A ${ship}...`)
+        await createTypeWriterText(menu, `SENDING CONTACT SIGNAL TO ${ship}...`)
         await createTypeWriterText(menu, `...`, { speed: 500 })
         await createTypeWriterText(menu, `...`, { speed: 500 })
+
         if (success) {
-            await createTypeWriterText(menu, `CANALE APERTO`, { style: `color: green` })
+            await createTypeWriterText(menu, `CHANNEL OPEN`, { style: `color: green` })
         } else {
             playSfx("errorsfx")
-            await createTypeWriterText(menu, `CONTATTO NON RIUSCITO`, { style: `color: red` })
+            await createTypeWriterText(menu, `CONTACT FAILED`, { style: `color: red` })
         }
-        await createTypeWriterMenu(menu, "< INDIETRO", menuHome)
+
+        await createTypeWriterMenu(menu, "< BACK", menuHome)
     })
 }
 
 // HELPERS
 const toggleAirlock1 = function (flag) {
-    showError("BLOCCO MANUALE IN FUNZIONE. DISINSERIRE BLOCCO MANUALE.")
+    showError("MANUAL LOCK ENGAGED. DISENGAGE MANUAL LOCK.")
 }
 
 const toggleAirlock2 = function (flag) {
@@ -266,8 +276,7 @@ const toggleMineshaft = function (flag) {
 
 const toggleShower = function (index) {
     if (index === 4) {
-        // questa doccia non funziona
-        showError("[DOCCIA 5 FUORI SERVIZIO.]")
+        showError("[SHOWER 5 OUT OF SERVICE.]")
     } else {
         context.showers[index] = !context.showers[index]
         menuShowers()
@@ -285,7 +294,7 @@ const toggleAdmin = function () {
         admin_el.innerHTML = 'GUEST'
     }
 
-    navigator.vibrate(200); // vibrate for 200ms
+    navigator.vibrate(200);
 }
 
 const toggleSelfDestructConfirm = function (index) {
@@ -294,11 +303,10 @@ const toggleSelfDestructConfirm = function (index) {
     if (isSelfDestructActivated()) {
         navigator.vibrate([
             100, 30, 100, 30, 100, 30, 200, 30, 200, 30, 200, 30, 100, 30, 100, 30, 100,
-        ]); // Vibrate 'SOS' in Morse.        
+        ]);
     } else {
-        navigator.vibrate(200); // vibrate for 200ms
+        navigator.vibrate(200);
     }
 
     menuSelfDestruct()
 }
-//
